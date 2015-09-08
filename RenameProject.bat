4dc9a7774d2ddd7c2dd4644b180e59a1a5ca7fac
StephenCWills <swills@gridprotectionalliance.org>
2015-09-08 17:24:10 -0400

    Added rename script for custom sand box projects.

---
+++ RenameProject.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SET target=%1
+SET source=%2
+IF /I "%source%"=="" SET source=openXDA_SdBx
+IF /I "%target%"=="" SET /p target="Enter project name: "
+IF /I "%target%"=="" (
+    ECHO ERROR: Please enter project name.
+    EXIT
+)
+ECHO.
+ECHO *** openXDA_SdBx Rename Script ***
+ECHO.
+ECHO About to rename "%source%" to "%target%", press Ctrl+C to cancel, or
+PAUSE
+.\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Build\*.*" %source% %target%
+.\Source\Dependencies\GSF\ReplaceInFiles /r /v /c ".\Source\*.*" %source% %target%
+.\Source\Dependencies\GSF\BRC64 /REPLACECI:%source%:%target% /RECURSIVE /EXECUTE
+ECHO.
+ECHO Project Rename Complete.
+ECHO.
