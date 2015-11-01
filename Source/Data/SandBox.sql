4a62ed0b8df5f29b0e99a6c290749ade7307f046
StephenCWills <swills@gridprotectionalliance.org>
2015-11-01 13:55:24 -0500

    Added example table definition to SandBox.sql.

--- Source/Data/SandBox.sql
+++ Source/Data/SandBox.sql
@@ -1,5 +1,14 @@
-USE [openXDA]
-GO
-
 -- Add custom data tables
 -- for analysis results here
+
+-- Example:
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'SandBoxResult')
+BEGIN
+	CREATE TABLE SandBoxResult
+	(
+		ID INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+        EventID INT NOT NULL REFERENCES Event(ID),
+        MyResult FLOAT NOT NULL
+	)
+END
+GO
