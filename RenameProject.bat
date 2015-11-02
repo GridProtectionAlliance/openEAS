6be77e4ec75fdc4cd4b24388d20c1de802b2c76b
StephenCWills <swills@gridprotectionalliance.org>
2015-11-02 08:32:48 -0500

    Fixed RenameProject.bat script.

--- RenameProject.bat
+++ RenameProject.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 SET target=%1
 SET source=%2
-IF /I "%source%"=="" SET source=openXDA_SdBx
+IF /I "%source%"=="" SET source=openESA
 IF /I "%target%"=="" SET /p target="Enter project name: "
 IF /I "%target%"=="" (
     ECHO ERROR: Please enter project name.
     EXIT
 )
 ECHO.
-ECHO *** openXDA_SdBx Rename Script ***
+ECHO *** openESA Rename Script ***
 ECHO.
 ECHO About to rename "%source%" to "%target%", press Ctrl+C to cancel, or
 PAUSE
