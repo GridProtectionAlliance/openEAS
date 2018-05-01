ecd658bf04602b8591dc07ff00953b718d61738d
Billy Ernest <bernest@gridprotectionalliance.org>
2018-05-01 10:37:42 -0400

    Added model for results

---
+++ Source/Libraries/openEASSandBox/OpenEASResult.cs
@@ -0,0 +1,13 @@
+﻿using GSF.Data.Model;
+
+namespace openEASSandBox
+{
+    public class OpenEASResult
+    {
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+        public int EventID { get; set; }
+        public double MyResult { get; set; }
+
+    }
+}
